2e632cd238f300378040fbfaa1e23f2d9055f9ce
Sivasomasekaran <34641391+Sivasomasekaran@users.noreply.github.com>
2018-03-03 21:46:00 +0530

    Create print k oddno.java

---
+++ C-Programming/Beginner Level/12set/print k oddno.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class oddre{
+    public static void main(String aa[]){
+   
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+    int count=1;
+    int k=s.nextInt();
+   for(int i=0;i<=a;i++){
+       if(i%2!=0){
+            if(count==k){
+               System.out.print(i);
+
+           }
+            count++;
+       }
+
+}
+}
+}
