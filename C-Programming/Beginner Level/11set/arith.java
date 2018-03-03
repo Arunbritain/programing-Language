ce073ee67c6b1180807caac5f7284aa77e9d286b
Sivasomasekaran <34641391+Sivasomasekaran@users.noreply.github.com>
2018-03-03 19:57:25 +0530

    Create arith.java

---
+++ C-Programming/Beginner Level/11set/arith.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class opp4{
+    public static void main(String aa[]){
+   
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+    int b=s.nextInt();
+    int c=s.nextInt();
+    int d=(a*b)/c;
+    System.out.println(d);
+           
+   }
+}
