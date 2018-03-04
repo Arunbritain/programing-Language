f7df253fd4220376612a35132fdec78b9670ac6d
Sivasomasekaran <34641391+Sivasomasekaran@users.noreply.github.com>
2018-03-04 20:34:09 +0530

    Create assccend.java

---
+++ C-Programming/playerlevel/3set/assccend.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.lang.String;
+
+
+public class wordsor{
+        public static void main(String args[]) {
+         String c;
+         Scanner s=new Scanner(System.in);
+         c=s.nextLine();
+         ArrayList<String>v=new ArrayList<String>();
+         String a[]=c.split(" ");
+         for(int i=0;i<a.length;i++){
+             v.add(a[i]);
+         }
+         Collections.sort(v);
+         for(String b:v){
+             System.out.printf("%s ", b);
+         }
+
+    }
+}
